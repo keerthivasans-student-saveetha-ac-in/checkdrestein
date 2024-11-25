226dce64e919fbcdb3d875dde0b5ae201eb4e25d
KEERTHIVASAN S <keerthivasans.student@saveetha.ac.in>
2024-11-25 08:53:32 +0000

    Registration Closed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -226,7 +226,7 @@ function Main() {
       </HeaderBg>
       <Link to="/form">
         <RegisterNow className="button">
-          <HoverSpan className="span_btn">REGISTERATION CLOSED</HoverSpan>
+          <HoverSpan className="span_btn">REGISTRATION CLOSED</HoverSpan>
         </RegisterNow>
       </Link>
       <Link to="/events">
